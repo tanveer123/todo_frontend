5c0364222bfea76afffc9110b10e2ad5b638e3e8
tanveer123 <tanveer_411393@hotmail.com>
2024-02-11 23:16:59 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
     // Fetching message from backend on mount
     useEffect(() => {
-        fetch("https://todo-backend.vercel.app/")
+        fetch("https://todo-backend.vercel.app")
             .then((res) => res.json())
             .then((data) => setMessage(data.message));
     }, []);
